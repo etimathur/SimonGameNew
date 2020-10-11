6ec18acdc021f58a2b67a83fa4f912976f37319f
Dimple13 <59231474+Dimple13@users.noreply.github.com>
2020-10-11 23:55:32 +0530

    blinking

--- app/src/main/java/com/example/simongame/MainActivity.java
+++ app/src/main/java/com/example/simongame/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.simongame;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 
+import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
@@ -12,6 +15,7 @@ import android.widget.ImageView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
@@ -24,26 +28,26 @@ import static java.lang.Thread.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    int noOfRows=3,noOfColumns=3;
     int[] myImageList;
     ArrayList<Integer> list;
     ImageView[] imageViews;
+    ImageView[] selected=new ImageView[100];
     final Handler handler = new Handler();
     int blinkDelay=0;
-
+    int[] answer = new int[100];
+    List clickedImageTags=new ArrayList();
+    int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         levelcheck();
-
     }
     public void levelcheck() {
         generateImages();
     }
     public void generateImages(){
-
         myImageList = new int[]{R.drawable.circleorange, R.drawable.circlegreen, R.drawable.circleyellow,R.drawable.circleorange, R.drawable.circlegreen, R.drawable.circleyellow,R.drawable.circleorange, R.drawable.circlegreen, R.drawable.circleyellow};
         list=new ArrayList<>();
         for(int i=0;i<myImageList.length;i++)
@@ -56,13 +60,37 @@ public class MainActivity extends AppCompatActivity {
         {
             imageViews[i].setImageResource(list.get(i));
         }
+
             BlinkingImages();
+
+    }
+    public void select(View view)
+    {
+        ImageView counter=(ImageView)view;
+        selected[i]=counter;
+        int taggedCounter=Integer.parseInt(counter.getTag().toString());
+        if (clickedImageTags.isEmpty())
+        {
+            counter.setBackgroundResource(R.drawable.squareselect);
+            clickedImageTags.add(taggedCounter);
+            i++;
+        }
+        else
+        {
+            int x=i-1;
+            selected[x].setBackgroundColor(Color.WHITE);
+            counter.setBackgroundResource(R.drawable.squareselect);
+            clickedImageTags.add(taggedCounter);
+            i++;
+
+        }
     }
     public void BlinkingImages()  {
         int a=4;
         for(int i=1;i<=a;i++){
             Random randomNum = new Random();
             final int num = randomNum.nextInt(8);
+            answer[i-1]=num;
             Log.i("Image", String.valueOf(num));
             handler.postDelayed(new Runnable() {
 
@@ -72,11 +100,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }, 1000 * i);
         }
-
+        Arrays.fill(answer, 0);
     }
-
     public void blink(final int num) {
-
         Log.i("Blink","now"+String.valueOf(num));
         final ImageView image=imageViews[num];
         image.setAlpha(0);
