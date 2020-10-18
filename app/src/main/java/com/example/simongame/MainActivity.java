14dd5bb22fefadea72637e17ef1eb7675b22a2b9
Dimple13 <59231474+Dimple13@users.noreply.github.com>
2020-10-18 17:29:06 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/simongame/MainActivity.java
+++ app/src/main/java/com/example/simongame/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.simongame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,6 +13,7 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -34,9 +36,12 @@ public class MainActivity extends AppCompatActivity {
     ImageView[] selected=new ImageView[100];
     final Handler handler = new Handler();
     int blinkDelay=0;
-    int[] answer = new int[100];
     List clickedImageTags=new ArrayList();
     int i=0;
+    ArrayList<Integer> selans=new ArrayList<>();
+    ArrayList<Integer> answer=new ArrayList<>();
+    private static final String TAG = "MainActivity";
+    int outLoop=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,28 +52,39 @@ public class MainActivity extends AppCompatActivity {
     public void levelcheck() {
         generateImages();
     }
-    public void generateImages(){
-        myImageList = new int[]{R.drawable.circleorange, R.drawable.circlegreen, R.drawable.circleyellow,R.drawable.circleorange, R.drawable.circlegreen, R.drawable.circleyellow,R.drawable.circleorange, R.drawable.circlegreen, R.drawable.circleyellow};
-        list=new ArrayList<>();
-        for(int i=0;i<myImageList.length;i++)
-        {
+    public void generateImages() {
+        myImageList = new int[]{R.drawable.circleorange, R.drawable.circlegreen, R.drawable.circleyellow, R.drawable.circleorange, R.drawable.circlegreen, R.drawable.circleyellow, R.drawable.circleorange, R.drawable.circlegreen, R.drawable.circleyellow};
+        list = new ArrayList<>();
+        for (int i = 0; i < myImageList.length; i++) {
             list.add(myImageList[i]);
         }
         Collections.shuffle(list);
-        imageViews=new ImageView[]{findViewById(R.id.v0),findViewById(R.id.v1),findViewById(R.id.v2),findViewById(R.id.v3),findViewById(R.id.v4),findViewById(R.id.v5),findViewById(R.id.v6),findViewById(R.id.v7),findViewById(R.id.v8)};
-        for(int i =0;i<9; i++)
-        {
+        imageViews = new ImageView[]{findViewById(R.id.v0), findViewById(R.id.v1), findViewById(R.id.v2), findViewById(R.id.v3), findViewById(R.id.v4), findViewById(R.id.v5), findViewById(R.id.v6), findViewById(R.id.v7), findViewById(R.id.v8)};
+        for (int i = 0; i < 9; i++) {
             imageViews[i].setImageResource(list.get(i));
         }
+        handler.postDelayed(new Runnable() {
 
-            BlinkingImages();
+            @Override
+            public void run() {
+                outLoop++;
+                BlinkingImages(outLoop);
+                Log.d(TAG, "run: "+outLoop);
+            }
+        }, 1000 );
+
+
+
+
 
     }
     public void select(View view)
     {
+
         ImageView counter=(ImageView)view;
         selected[i]=counter;
         int taggedCounter=Integer.parseInt(counter.getTag().toString());
+        selans.add(taggedCounter);
         if (clickedImageTags.isEmpty())
         {
             counter.setBackgroundResource(R.drawable.squareselect);
@@ -84,13 +100,29 @@ public class MainActivity extends AppCompatActivity {
             i++;
 
         }
+        Log.d(TAG, "select:"+selans);
+        Log.d(TAG, "answer:"+answer);
+        for(int i=0;i<selans.size();i++){
+            if(selans.get(i)!=answer.get(i)){
+                Log.d(TAG, "select: not same");
+                Toast.makeText(MainActivity.this,"Wrong answer",Toast.LENGTH_SHORT).show();
+                Intent go=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(go);
+                break;
+            }
+            if(selans.size()==answer.size()){
+                Toast.makeText(MainActivity.this,"Correct answer",Toast.LENGTH_SHORT).show();
+                outLoop++;
+                BlinkingImages(outLoop);
+            }
+        }
+
     }
-    public void BlinkingImages()  {
-        int a=4;
-        for(int i=1;i<=a;i++){
+    public void BlinkingImages(int count)  {
+        for(int i=1;i<=count;i++){
             Random randomNum = new Random();
             final int num = randomNum.nextInt(8);
-            answer[i-1]=num;
+            answer.add(num);
             Log.i("Image", String.valueOf(num));
             handler.postDelayed(new Runnable() {
 
@@ -100,7 +132,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }, 1000 * i);
         }
-        Arrays.fill(answer, 0);
+//        Arrays.fill(answer, 0);
+
     }
     public void blink(final int num) {
         Log.i("Blink","now"+String.valueOf(num));
